9dca8f23533adbb37f03f36e448c3a03adb242db
Annie <annie.chien105@gmail.com>
2022-10-20 16:14:12 +0800

    change background-iamge

--- src/components/Header/StyledHeader.js
+++ src/components/Header/StyledHeader.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
-
+import {MEDIA_QUERY} from '../../constants'
 
 export const StyledHeader = styled.header`
   height: 20vh;
   background-image: url('images/bg-header-mobile.svg');
   background-color: var(--color-primary-cyan);
+
+  @media (min-width: ${MEDIA_QUERY.DESKTOP}) {
+    background-image: url('images/bg-header-desktop.svg');
+  }
 `
